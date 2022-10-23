439c8be3e783d453471fcf0e5387b2a0f0966771
Code Sandbox <hello@codesandbox.io>
2022-10-23 17:33:16 +0000

    Deployment from Codesandbox.

--- car.js
+++ car.js
@@ -3,36 +3,36 @@
 
   // Physics
 
-  const maxPower = 0.075;
-  const maxReverse = 0.0375;
-  const powerFactor = 0.001;
-  const reverseFactor = 0.0005;
+  const maxPower = 2;
+  const maxReverse = 0.5;
+  const powerFactor = 2;
+  const reverseFactor = 0.005;
 
-  const drag = 0.95;
-  const angularDrag = 0.95;
-  const turnSpeed = 0.002;
+  const drag = 0.5;
+  const angularDrag = 0.5;
+  const turnSpeed = 0.05;
 
   const WIDTH = 1500;
   const HEIGHT = 1500;
 
-  const $canvas = document.querySelector('canvas');
+  const $canvas = document.querySelector("canvas");
 
   $canvas.width = WIDTH;
   $canvas.height = HEIGHT;
 
-  const ctx = $canvas.getContext('2d');
+  const ctx = $canvas.getContext("2d");
 
-  ctx.fillStyle = 'hsla(0, 0%, 25%, 0.25)';
+  ctx.fillStyle = "hsla(0, 0%, 25%, 0.25)";
 
-  const $scene = document.querySelector('.scene');
-  const $cars = document.querySelector('.cars');
-  const $map = document.querySelector('.map');
-  const $bullets = document.querySelector('.bullets');
+  const $scene = document.querySelector(".scene");
+  const $cars = document.querySelector(".cars");
+  const $map = document.querySelector(".map");
+  const $bullets = document.querySelector(".bullets");
 
-  const $points = document.querySelector('.points');
+  const $points = document.querySelector(".points");
 
   const localCar = {
-    $el: document.querySelector('.car'),
+    $el: document.querySelector(".car"),
     x: WIDTH / 2,
     y: HEIGHT / 2,
     xVelocity: 0,
@@ -55,7 +55,7 @@
   const cars = [localCar];
   const carsById = {};
 
-  if (window.location.search === '?test') {
+  if (window.location.search === "?test") {
     cars.push({ ...localCar });
     cars[1].$el = cars[0].$el.cloneNode(true);
     cars[0].$el.parentNode.appendChild(cars[1].$el);
@@ -63,7 +63,7 @@
 
   const bullets = [];
 
-  function updateCar (car, i) {
+  function updateCar(car, i) {
     if (car.isHit || car.isShot) {
       if (car === localCar) {
         car.isHit = false;
@@ -126,7 +126,7 @@
     }
   }
 
-  function update () {
+  function update() {
     cars.forEach(updateCar);
 
     for (let i = 0; i < bullets.length; i++) {
@@ -146,15 +146,16 @@
 
     const canTurn = localCar.power > 0.0025 || localCar.reverse;
 
-    const controls = localCar.name != null
-      ? window.getControls()
-      : {
-          up: 0,
-          left: 0,
-          right: 0,
-          down: 0,
-          shoot: 0
-        };
+    const controls =
+      localCar.name != null
+        ? window.getControls()
+        : {
+            up: 0,
+            left: 0,
+            right: 0,
+            down: 0,
+            shoot: 0
+          };
 
     const throttle = Math.round(controls.up * 10) / 10;
     const reverse = Math.round(controls.down * 10) / 10;
@@ -165,7 +166,10 @@
       localCar.isShooting = isShooting;
     }
 
-    if (localCar.isThrottling !== throttle || localCar.isReversing !== reverse) {
+    if (
+      localCar.isThrottling !== throttle ||
+      localCar.isReversing !== reverse
+    ) {
       changed = true;
       localCar.isThrottling = throttle;
       localCar.isReversing = reverse;
@@ -209,7 +213,12 @@
         continue;
       }
 
-      if (circlesHit({ x: car.x, y: car.y, r: 7.5 }, { x: localCar.x, y: localCar.y, r: 7.5 })) {
+      if (
+        circlesHit(
+          { x: car.x, y: car.y, r: 7.5 },
+          { x: localCar.x, y: localCar.y, r: 7.5 }
+        )
+      ) {
         localCar.isHit = true;
         changed = true;
       }
@@ -221,7 +230,13 @@
       for (let i = 0; i < bullets.length; i++) {
         const bullet = bullets[i];
 
-        if (bullet && circlesHit({ x: car.x, y: car.y, r: 7.5 }, { x: bullet.x, y: bullet.y, r: 2 })) {
+        if (
+          bullet &&
+          circlesHit(
+            { x: car.x, y: car.y, r: 7.5 },
+            { x: bullet.x, y: bullet.y, r: 2 }
+          )
+        ) {
           if (car !== localCar) {
             if (!car.isShot) {
               car.isShot = true;
@@ -256,53 +271,64 @@
     }
   }, 1000 / 120);
 
-  function renderCar (car, index) {
+  function renderCar(car, index) {
     const { x, y, angle, power, reverse, angularVelocity } = car;
 
     if (!car.$body) {
-      car.$body = car.$el.querySelector('.car-body');
+      car.$body = car.$el.querySelector(".car-body");
     }
 
     if (!car.$name) {
-      car.$name = car.$el.querySelector('.car-name');
+      car.$name = car.$el.querySelector(".car-name");
     }
 
     car.$el.style.transform = `translate(${x}px, ${y}px)`;
-    car.$body.style.transform = `rotate(${angle * 180 / Math.PI}deg)`;
-    car.$name.textContent = car.name || '';
+    car.$body.style.transform = `rotate(${(angle * 180) / Math.PI}deg)`;
+    car.$name.textContent = car.name || "";
 
     if (car.isShot) {
-      car.$body.classList.add('shot');
+      car.$body.classList.add("shot");
     } else {
-      car.$body.classList.remove('shot');
+      car.$body.classList.remove("shot");
     }
 
-    if ((power > 0.0025) || reverse) {
-      if (((maxReverse === reverse) || (maxPower === power)) && Math.abs(angularVelocity) < 0.002) {
+    if (power > 0.0025 || reverse) {
+      if (
+        (maxReverse === reverse || maxPower === power) &&
+        Math.abs(angularVelocity) < 0.002
+      ) {
         return;
       }
       ctx.fillRect(
-        x - Math.cos(angle + 3 * Math.PI / 2) * 3 + Math.cos(angle + 2 * Math.PI / 2) * 3,
-        y - Math.sin(angle + 3 * Math.PI / 2) * 3 + Math.sin(angle + 2 * Math.PI / 2) * 3,
+        x -
+          Math.cos(angle + (3 * Math.PI) / 2) * 3 +
+          Math.cos(angle + (2 * Math.PI) / 2) * 3,
+        y -
+          Math.sin(angle + (3 * Math.PI) / 2) * 3 +
+          Math.sin(angle + (2 * Math.PI) / 2) * 3,
         1,
         1
       );
       ctx.fillRect(
-        x - Math.cos(angle + 3 * Math.PI / 2) * 3 + Math.cos(angle + 4 * Math.PI / 2) * 3,
-        y - Math.sin(angle + 3 * Math.PI / 2) * 3 + Math.sin(angle + 4 * Math.PI / 2) * 3,
+        x -
+          Math.cos(angle + (3 * Math.PI) / 2) * 3 +
+          Math.cos(angle + (4 * Math.PI) / 2) * 3,
+        y -
+          Math.sin(angle + (3 * Math.PI) / 2) * 3 +
+          Math.sin(angle + (4 * Math.PI) / 2) * 3,
         1,
         1
       );
     }
 
     if (car !== localCar) {
-      const angle = Math.atan2((car.y - localCar.y), (car.x - localCar.x));
+      const angle = Math.atan2(car.y - localCar.y, car.x - localCar.x);
 
       let $mapitem = $map.childNodes[index - 1];
 
       if (!$mapitem) {
-        $mapitem = document.createElement('div');
-        $mapitem.classList.add('map-item');
+        $mapitem = document.createElement("div");
+        $mapitem.classList.add("map-item");
         $map.appendChild($mapitem);
       }
 
@@ -313,15 +339,16 @@
     }
   }
 
-  function render (ms) {
+  function render(ms) {
     requestAnimationFrame(render);
 
     $points.textContent = cars
       .slice()
       .sort((a, b) => (b.points || 0) - (a.points || 0))
-      .map(car => {
-        return [car.name || 'anonymous', car.points || 0].join(': ');
-      }).join('\n');
+      .map((car) => {
+        return [car.name || "anonymous", car.points || 0].join(": ");
+      })
+      .join("\n");
 
     cars.forEach(renderCar);
 
@@ -335,8 +362,8 @@
       const bullet = bullets[i];
       const { x, y, shootAt } = bullet;
       if (!bullet.$el) {
-        const $el = bullet.$el = document.createElement('div');
-        $el.classList.add('bullet');
+        const $el = (bullet.$el = document.createElement("div"));
+        $el.classList.add("bullet");
         $bullets.appendChild($el);
       }
       bullet.$el.style.transform = `translate(${x}px, ${y}px)`;
@@ -357,30 +384,30 @@
 
   requestAnimationFrame(render);
 
-  const socket = io('https://car.pakastin.fi', {
+  const socket = io("https://car.pakastin.fi", {
     withCredentials: true
   });
 
-  socket.on('connect', () => {
+  socket.on("connect", () => {
     sendParams(localCar);
   });
 
-  socket.on('join', () => {
+  socket.on("join", () => {
     sendParams(localCar);
   });
 
-  socket.on('params', ({ id, params }) => {
+  socket.on("params", ({ id, params }) => {
     let car = carsById[id];
 
     if (!car) {
-      const $el = document.createElement('div');
-      $el.classList.add('car');
-      const $body = document.createElement('div');
-      $body.classList.add('car-body');
-      const $roof = document.createElement('div');
-      $roof.classList.add('car-roof');
-      const $name = document.createElement('div');
-      $name.classList.add('car-name');
+      const $el = document.createElement("div");
+      $el.classList.add("car");
+      const $body = document.createElement("div");
+      $body.classList.add("car-body");
+      const $roof = document.createElement("div");
+      $roof.classList.add("car-roof");
+      const $name = document.createElement("div");
+      $name.classList.add("car-name");
       $body.appendChild($roof);
       $el.appendChild($body);
       $el.appendChild($name);
@@ -393,17 +420,17 @@
     }
 
     for (const key in params) {
-      if (key !== 'el') {
+      if (key !== "el") {
         car[key] = params[key];
       }
     }
   });
 
-  socket.on('leave', (id) => {
+  socket.on("leave", (id) => {
     const car = carsById[id];
 
     if (!car) {
-      return console.error('Car not found');
+      return console.error("Car not found");
     }
 
     for (let i = 0; i < cars.length; i++) {
@@ -419,7 +446,7 @@
     delete carsById[id];
   });
 
-  function sendParams (car) {
+  function sendParams(car) {
     const {
       x,
       y,
@@ -440,7 +467,7 @@
       points
     } = car;
 
-    socket.emit('params', {
+    socket.emit("params", {
       x,
       y,
       xVelocity,
@@ -461,12 +488,12 @@
     });
   }
 
-  const $disconnect = document.querySelector('.disconnect');
+  const $disconnect = document.querySelector(".disconnect");
 
   $disconnect.onclick = () => {
     socket.disconnect();
 
-    localCar.name = '';
+    localCar.name = "";
 
     while (cars.length > 1) {
       const car = cars.pop();
@@ -477,28 +504,28 @@
     $disconnect.parentNode.removeChild($disconnect);
   };
 
-  const $clearScreen = document.querySelector('.clearscreen');
+  const $clearScreen = document.querySelector(".clearscreen");
 
   $clearScreen.onclick = () => {
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
   };
 
   setInterval(() => {
-    ctx.fillStyle = 'hsla(0, 0%, 95%, 0.2)';
+    ctx.fillStyle = "hsla(0, 0%, 95%, 0.2)";
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
-    ctx.fillStyle = 'hsla(0, 0%, 25%, 0.5)';
+    ctx.fillStyle = "hsla(0, 0%, 25%, 0.5)";
   }, 15 * 1000);
 
-  function circlesHit ({ x: x1, y: y1, r: r1 }, { x: x2, y: y2, r: r2 }) {
-    return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) < (r1 + r2);
+  function circlesHit({ x: x1, y: y1, r: r1 }, { x: x2, y: y2, r: r2 }) {
+    return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) < r1 + r2;
   }
 
-  const $name = document.querySelector('.name');
+  const $name = document.querySelector(".name");
 
-  $name.querySelector('form').onsubmit = (e) => {
+  $name.querySelector("form").onsubmit = (e) => {
     e.preventDefault();
 
-    localCar.name = $name.querySelector('input').value || '';
+    localCar.name = $name.querySelector("input").value || "";
 
     $name.parentNode.removeChild($name);
   };
